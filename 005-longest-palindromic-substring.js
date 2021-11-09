3addc98d3580351b93328998610ccd632acb6e12
Rich Sturim <rich@sturim.org>
2021-11-09 07:20:04 -0500

    start of 005

---
+++ 005-longest-palindromic-substring.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  const result = [];
+
+  return result;
+};
+
+const s = 'babad';
+// Output: "bab"
+// Note: "aba" is also a valid answer.
+
+// const s = "cbbd"
+// Output: "bb"
+
+// const s = "a"
+// Output: "a"
+
+// const s = "ac"
+// Output: "a"
+
+const result = longestPalindrome(s);
+console.log('result: ', result);
