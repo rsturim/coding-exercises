b3e0b0073f1bdc91750cbd935611bdee1e00225a
Rich Sturim <rich@sturim.org>
2021-11-09 06:33:33 -0500

    update matrix

--- walk-the-matrix.js
+++ walk-the-matrix.js
@@ -1,73 +1,69 @@
-/**
- * @param {List[List[int]]} matrix
- * @return {List[int]}
- */
 const walkMatrix = (matrix) => {
-  const visited = matrix.map((row) => row.map((col) => false));
-
-  const rowCount = matrix[0].length;
-  const colCount = matrix.length;
-  const finalLength = rowCount * colCount;
-
   const result = [];
 
+  // Initialize the upper and lower bounds indexes
+  const rowCount = matrix.length;
+  const columnCount = matrix[0].length;
+  const finalSize = rowCount * columnCount;
   let startRow = 0;
   let endRow = rowCount - 1;
   let startColumn = 0;
-  let endColumn = colCount - 1;
+  let endColumn = columnCount - 1;
 
+  // We will keep walking as long as rows or columns remain.
+  // That happens as long as the end is greater or equal to the start.
   // while (endRow >= startRow && endColumn >= startColumn) {
-  while (result.length < finalLength) {
-    // traverse top
+  while (result.length < finalSize) {
+    // Walk across the top starting row for each column from beginning to end
+    // This is left-right across the top
     for (let column = startColumn; column <= endColumn; column++) {
-      const item = matrix[startRow][column];
-      result.push(item);
+      // Add the item to result in order
+      result.push(matrix[startRow][column]);
     }
-    // We have walk across the entirety of the starting row, so increment the index.
+
+    // Increment our start row since we visited each value
     startRow++;
 
-    // Walk down (right column)
+    // Walk top-bottom for the end column
     for (let row = startRow; row <= endRow; row++) {
-      const item = matrix[row][endColumn];
-      result.push(item);
+      result.push(matrix[row][endColumn]);
     }
-    // We have walked across end column, so we decrement the index.
     endColumn--;
 
-    // Walk left (bottom row)
-    for (let column = endColumn; column >= startColumn; column--) {
-      const item = matrix[endRow][column];
-      result.push(item);
+    // Since we increment startRow, we need to make sure we are still in bounds
+    if (endRow >= startRow) {
+      //  Walk left-right on the bottom row
+      for (let column = endColumn; column >= startColumn; column--) {
+        result.push(matrix[endRow][column]);
+      }
     }
-    // We have walked across end row, so we decrement the index.
     endRow--;
 
-    // Walk up (left column)
-    for (let row = endRow; row >= startRow; row--) {
-      const item = matrix[row][startColumn];
-      result.push(item);
+    // Since we increment startRow, we need to make sure we are still in bounds
+    if (endColumn >= startColumn) {
+      // walk bottom-top for the start column
+      for (let row = endRow; row >= startRow; row--) {
+        result.push(matrix[row][startColumn]);
+      }
     }
-    // We have walked across the starting column, so we increment the index
     startColumn++;
   }
 
   return result;
 };
-
 // Input
-// const matrix = [
-//   [0, 1, 2, 3],
-//   [11, 12, 13, 4],
-//   [10, 15, 14, 5],
-//   [9, 8, 7, 6],
-// ];
-
 const matrix = [
-    [0, 1],
-    [5, 2],
-    [4, 3],
-  ];
-  
+  [0, 1, 2, 3],
+  [11, 12, 13, 4],
+  [10, 15, 14, 5],
+  [9, 8, 7, 6],
+];
+
+// const matrix = [
+//   [0, 1],
+//   [5, 2],
+//   [4, 3],
+// ];
 
 // Output: walkMatrix(matrix)
 //   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
